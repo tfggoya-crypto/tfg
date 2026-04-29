6dbf2be76471fba3e3db5f27582592f5a9796695
Alvaro Jose Hernandez Sanchez <hernandezsanchezalvarojose@gmail.com>
2026-04-29 20:53:39 +0200

    Creacion de middleware de admin

--- fincas/routes/web.php
+++ fincas/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PropietarioController;
 use App\Http\Controllers\EmpleadoController;
+use App\Http\Middleware\RoleAdminMiddleware;
 
 // Página principal
 Route::get('/', fn() => view('welcome'));
@@ -15,7 +16,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Admin
-Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index'])->middleware(RoleAdminMiddleware::class);
 
 // Propietario
 Route::get('/propietario', [PropietarioController::class, 'index']);
